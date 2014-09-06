80698ddd22a36b9c432fcbf4ecb1b189b3988b09
Joaquin <joaduo@gmail.com>
2014-09-06 00:53:10 -0400

    Add documentation. Enhance behavior for running __init__.py as main

--- README.md
+++ README.md
@@ -1,4 +1,89 @@
-relative_import
-===============
+The relative_import utility
+===========================
 
 Relative import tool for running subpackages or submodules as main scripts (enabling relative imports)
+
+
+How do you use it?
+------------------
+Imagine you have 2 modules inside a package called "my_pkg".
+
+That would be:
+
+* my_pkg/math_lib.py
+* my_pkg/test.py
+
+So in test.py we could have
+
+```python
+from .math_lib import factorize
+
+def print_factorize(number):
+    num = factorize(number)
+    print num
+
+if __name__ == '__main__':
+    #Small smoke test
+    factorize_and_print(10)
+    factorize_and_print(0)
+    factorize_and_print(-10)
+```
+If you do "python my_pkg/test.py" it will throw an exception because of the relative import at the first line.
+
+PEP 366 presents a workaround like:
+```python
+if __name__ == "__main__" and __package__ is None:
+    __package__ = "my_pkg"
+
+from .math_lib import factorize
+
+```
+
+This will make the code work, but it is not an elegant solution. 
+
+So you can use **relative_import** to make your code look nicer like.
+```python
+import relative_import
+from .math_lib import factorize
+
+```
+
+How does it work?
+-----------------
+
+The solution uses the same technique in PEP 366, but setting __package__ through dynamic inspection of the stack. To solve the value of __package__ it compares the current `__main__`'s file with paths at sys.path -or, optionally, a list of paths given in the preferences-.
+
+For example, for a file in **/home/user/projects/python/math/my_pkg/test.py** given the following paths in sys.path:
+```python
+[
+'/home/user/projects/python/',
+'/home/user/projects/python/math/',
+'/home/user/projects/python/math/my_pkg/',
+]
+```
+It will pick the closest path to the `__main__`'s file that is not the `__main__`'s file's directory.
+
+Then the base path use to solve __package__ variable will be **/home/user/projects/python/math/**
+
+Preferences
+-----------
+This package works out-of-the-box without any configuration, but you can set certain preferences.
+
+To do so, create a relative_import_settings.py in one of your PYTHON_PATHs. Should be like
+
+```python
+
+class Settings(object):
+    #Self explanatory
+    relative_import_enabled = True
+    #instead of searching in sys.path, use this list
+    #beware, if empty, it will look into sys.path anyway
+    relative_import_path = ['/some/path/to/project/']
+
+```
+
+Related PEPs
+------------
+
+* http://legacy.python.org/dev/peps/pep-0328/
+* http://legacy.python.org/dev/peps/pep-0366/
--- __init__.py
+++ __init__.py
@@ -7,6 +7,17 @@ This is useful for running smoke tests or small scripts within the module.
 
 This utility is not intended to be run on production code, rather to reduce
 developing cycles times.
+
+Usage:
+------
+
+To enable relative importing in a submodule ran as __main__, you simply import
+this package
+import relative_import
+
+Make sure your PYTHON_PATH is correctly set to solve the relative path of the 
+submodule/subpackage.
+
 '''
 
 from inspect import currentframe
@@ -16,21 +27,21 @@ import sys
 
 def __solve_import_paths():
     try:
-        from smoothtest_settings import Settings
+        from relative_import_settings import Settings
         settings = Settings()
         if not (settings and settings.relative_import_enabled):
             if not settings.relative_import_enabled:
                 print >> sys.stderr, 'Relative import not enabled in settings.'
             return
-        return settings.relative_import_paths or list(sys.path)
+        return settings.relative_import_path or list(sys.path)
     
     except ImportError:
         pass
 
-def __get_python_path(caller_dir, import_paths):
+def __get_python_path(caller_dir, import_path):
     paths = []
     #look for paths containing the file
-    for imp_pth in import_paths:
+    for imp_pth in import_path:
         imp_pth = path.abspath(imp_pth) 
         if (imp_pth != caller_dir
             and imp_pth == path.commonprefix((imp_pth, caller_dir))):
@@ -44,27 +55,50 @@ def __get_package_str(caller_dir, python_path):
     rel = path.relpath(caller_dir, python_path)
     return rel.replace(path.sep, '.')
 
-def __enable_relative_import(frames_back=2):
+def __enable_relative_import():
     #solve caller locals
     frame = currentframe()
-    for _ in range(frames_back):
+    #go to frames back to find who imported us
+    for _ in range(2):
         frame = frame.f_back
+    #now we have access to the locals :)
     caller_locals = frame.f_locals
+    #already set or not __main__, stop doing anything.
+    #In general this function won't be called twice (since it's only called
+    #the first time the package is loaded (but could be called the first
+    #time from one non __main__ module
+    if caller_locals['__package__'] or caller_locals['__name__'] != '__main__':
+        return 
     #solve settings
-    import_paths = __solve_import_paths()
+    import_path = __solve_import_paths()
+    #disabled or no import paths
+    if not import_path:
+        return
     #solve the relative path to one of the python paths provided
     caller_dir = caller_locals['__file__']
     caller_dir = path.dirname(path.abspath(caller_dir))
-    python_path = __get_python_path(caller_dir, import_paths)
+    python_path = __get_python_path(caller_dir, import_path)
     if not python_path:
         return
-    #solve package and import
+    #solve package name
     pkg_str = __get_package_str(caller_dir, python_path)
+    #import it
     try:
-        importlib.import_module(pkg_str)
+        if '__init__.py' in caller_locals['__file__']:
+            #The __main__ is its own package
+            #If we treat it as a normal module it would be imported twice
+            sys.modules[pkg_str] = sys.modules['__main__']
+            #We need to set __path__ because its needed for
+            #relative importing
+            sys.modules[pkg_str].__path__ = [caller_dir]
+        else:
+            #we need to import 
+            importlib.import_module(pkg_str)
         #finally enable relative import
         caller_locals['__package__'] = pkg_str
     except ImportError as e:
+        #In many situations we won't care if it fails
+        #it will fail anyway if it finds an relative import in __main__
         print >> sys.stderr, e 
 
 __enable_relative_import()
