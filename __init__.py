9ff039a4a64c0a46f22318229dca163d8f1686e4
Joaquin <joaduo@gmail.com>
2014-09-07 15:00:52 -0300

    PEP 8 fix

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 '''
 Copyright (c) 2014 Joaquin Duo - File under MIT License
 
-Import this module to enable relative importing on a submodule/subpackage 
-running it as a main module. 
+Import this module to enable relative importing on a submodule/subpackage
+running it as a main module.
 This is useful for running smoke tests or small scripts within the module.
 
 This utility is not intended to be run on production code, rather to reduce
@@ -15,7 +15,7 @@ To enable relative importing in a submodule ran as __main__, you simply import
 this package
 import relative_import
 
-Make sure your PYTHON_PATH is correctly set to solve the relative path of the 
+Make sure your PYTHON_PATH is correctly set to solve the relative path of the
 submodule/subpackage.
 
 '''
@@ -25,6 +25,7 @@ from os import path
 import importlib
 import sys
 
+
 def __get_search_path(main_file_dir, sys_path):
     #List of candidate search paths
     paths = []
@@ -33,18 +34,22 @@ def __get_search_path(main_file_dir, sys_path):
         #convert relative path to absolute
         imp_pth = path.abspath(imp_pth)
         #filter __main__'s file directory, naturally it will be in the sys.path
-        #filter parent paths containing the package 
+        #filter parent paths containing the package
         if (imp_pth != main_file_dir
             and imp_pth == path.commonprefix((imp_pth, main_file_dir))):
             paths.append(imp_pth)
-    
+    #check if we have results
     if paths:
         #we found candidates
         #now look for the largest parent search path
         paths.sort()
         return paths[-1]
 
+
 def __enable_relative_import():
+    '''
+    Enables explicit relative import in sub-modules when ran as __main__
+    '''
     #solve caller locals
     frame = currentframe()
     #go to frames back to find who imported us
@@ -76,13 +81,14 @@ def __enable_relative_import():
             #relative importing
             sys.modules[pkg_str].__path__ = [main_file_dir]
         else:
-            #we need to import 
+            #we need to import
             importlib.import_module(pkg_str)
         #finally enable relative import
         main_globals['__package__'] = pkg_str
     except ImportError as e:
         #In many situations we won't care if it fails
         #it will fail anyway if it finds an relative import in __main__
-        print >> sys.stderr, e 
+        print >> sys.stderr, e
+
 
 __enable_relative_import()
