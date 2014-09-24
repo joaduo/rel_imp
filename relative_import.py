c3c8471cb31b8dce7084a738401d67ea76cfb56a
jduo <joaduo@gmail.com>
2014-09-23 21:42:29 -0300

    Better docs

--- relative_import.py
+++ relative_import.py
@@ -1,20 +1,23 @@
 '''
 Copyright (c) 2014 Joaquin Duo - File under MIT License
 
-Import this module to enable relative importing on a submodule/subpackage
-running it as a main module.
-This is useful for running smoke tests or small scripts within the module.
+Import this module to enable explicit relative importing on a submodule or
+sub-package running it as a main module. Doing so is useful for running smoke 
+tests or small scripts within the module.
 
-This utility is not intended to be run on production code, rather to reduce
-developing cycles times.
+If you are using this tool enabled on production, make sure you do enough 
+testing. (since it does some guessing trying to find the right package of
+the module)
 
 Usage:
 ------
 
-To enable relative importing in a submodule ran as __main__, you simply import
-this package
+To enable explicit relative importing in __main__, you simply import
+this package before any relative import
 
 import relative_import
+
+from .my_pkg import foo, bar
 
 Make sure your PYTHON_PATH is correctly set to solve the relative path of the
 submodule/subpackage.
@@ -28,7 +31,7 @@ import sys
 
 
 def __get_search_path(main_file_dir, sys_path):
-    #List of candidate search paths
+    #Gather candidate search paths
     paths = []
     #look for paths containing the file
     for imp_pth in sys_path:
@@ -51,23 +54,24 @@ def __enable_relative_import():
     '''
     Enables explicit relative import in sub-modules when ran as __main__
     '''
-    #solve caller locals
+    #find caller locals
     frame = currentframe()
-    #go to frames back to find who imported us
+    #go two frames back to find who imported us
     for _ in range(2):
         frame = frame.f_back
-    #now we have access to the locals
-    #import ipdb; ipdb.set_trace()
+    #now we have access to the module globals
     main_globals = frame.f_globals
-    #already set or not __main__, stop doing anything.
-    #In general this function won't be called twice (since it's only called
-    #the first time the package is loaded (but could be called the first
-    #time from one non __main__ module
+
+    #If __package__ is already set or its not the __main__, stop doing anything.
+    # (in some cases relative_import could be called once from outside
+    # __main__ if it was not called in __main__)
+    # (also a reload of relative_import could trigger this function)
     if main_globals['__package__'] or main_globals['__name__'] != '__main__':
         return
-    #solve __main__'s file dir
+    #find __main__'s file directory
     main_file_dir = path.dirname(path.abspath(main_globals['__file__']))
     search_path = __get_search_path(main_file_dir, sys.path)
+    #no candidates for search path
     if not search_path:
         return
     #solve package name from search path
@@ -75,21 +79,23 @@ def __enable_relative_import():
     #import the package in order to set __package__ value later
     try:
         if '__init__.py' in main_globals['__file__']:
-            #The __main__ is its own package
+            #The __main__ is __init__.py => its own package
             #If we treat it as a normal module it would be imported twice
+            #So we simply reuse it
             sys.modules[pkg_str] = sys.modules['__main__']
             #We need to set __path__ because its needed for
             #relative importing
             sys.modules[pkg_str].__path__ = [main_file_dir]
         else:
-            #we need to import
+            #we need to import the package to be available
             importlib.import_module(pkg_str)
         #finally enable relative import
         main_globals['__package__'] = pkg_str
     except ImportError as e:
-        #In many situations we won't care if it fails
-        #it will fail anyway if it finds an relative import in __main__
+        #In many situations we won't care if it fails, simply report error
+        #main will fail anyway if finds an explicit relative import
         print >> sys.stderr, e
 
-
+#Enable relative import in __main__
+#this function will be called only the first import of this module (or reloads)
 __enable_relative_import()
