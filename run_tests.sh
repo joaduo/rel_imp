75f5ba479dcc83a320b184911edbde51ef41d03f
jduo <joaduo@gmail.com>
2014-09-24 23:58:35 -0300

    Automate packaging and testing

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+#Checking in current platform
 python -m unittest discover
+
+#test in wine
+wine python -m unittest discover
--- sdist.sh
+++ sdist.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
-pandoc --from=markdown --to=rst --output=README README.md
-python -m unittest discover && python setup.py sdist && python setup.py check -r
+ 
+source run_tests.sh
 
+#Convert markdown to rst
+pandoc --from=markdown --to=rst --output=README README.md
+
+#build package
+python setup.py sdist && python setup.py check -r
+
+#test installation
+mkdir venv -p
+cd venv
+virtualenv ./
+source bin/activate
+pip install ../dist/relative_import-0.1.1.tar.gz
+pip uninstall relative_import -y
+cd ..
+#rm venv -Rf
+
+#Leave message
 echo 
 echo "upload with: python setup.py sdist upload -r pypi"
