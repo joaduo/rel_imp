0470e5e2a96b483c64aefd9a92b859a40d72c0f0
jduo <joaduo@gmail.com>
2014-09-26 01:33:04 -0300

    New strategy for finding the search path. Better tests in windows. remove uneeded test

--- rel_imp.py
+++ rel_imp.py
@@ -23,11 +23,11 @@ Make sure your PYTHON_PATH is correctly set to solve the relative path of the
 submodule/subpackage.
 
 '''
-from __future__ import print_function
 from inspect import currentframe
 from os import path
 import importlib
 import sys
+import os
 
 
 def __get_search_path(main_file_dir, sys_path):
@@ -52,17 +52,47 @@ def __get_search_path(main_file_dir, sys_path):
 def __print_exc(e):
     msg = ('Exception enabling relative_import for __main__. Ignoring it: %r'
            '\n  relative_import won\'t be enabled.')
-    print(msg % e, file=sys.stderr)
+    __log_error(msg % e)
+    
+def __log_error(msg):
+    sys.stderr.write(msg + '\n')
+    sys.stderr.flush()
+
+def __try_search_paths(main_globals):
+    #try with abspath
+    #main_file_dir = path.dirname(path.abspath(main_globals['__file__']))
+    fl = main_globals['__file__']
+    search_path = None
+    if not path.isabs(fl):
+        #Build absolute path from PWD if possible
+        cwd = os.getenv('PWD', os.getcwd())
+        cwd_fl = path.abspath( path.join(cwd,fl))
+        main_dir = path.dirname(cwd_fl)
+        search_path = __get_search_path(main_dir, sys.path)
+    
+    if not search_path:
+        #try absolute strategy (should be the same...
+        main_dir = path.dirname(path.abspath(fl))
+        search_path = __get_search_path(main_dir, sys.path)
+    
+    if not search_path:
+        #try real path strategy
+        main_dir = path.dirname(path.realpath(fl))
+        sys_path = [path.realpath(p) for p in sys.path]
+        search_path = __get_search_path(main_dir, sys_path)
+        
+    return main_dir, search_path
+
 
 def __solve_pkg(main_globals):
-    #find __main__'s file directory
-    main_file_dir = path.dirname(path.abspath(main_globals['__file__']))
-    search_path = __get_search_path(main_file_dir, sys.path)
-    #no candidates for search path
+    #find __main__'s file directory and search path
+    main_dir, search_path = __try_search_paths(main_globals)
     if not search_path:
+        #no candidates for search path, return
         return
     #solve package name from search path
-    pkg_str = path.relpath(main_file_dir, search_path).replace(path.sep, '.')
+    pkg_str = path.relpath(main_dir, search_path).replace(path.sep, '.')
+    assert pkg_str
     #import the package in order to set __package__ value later
     try:
         if '__init__.py' in main_globals['__file__']:
@@ -72,7 +102,7 @@ def __solve_pkg(main_globals):
             sys.modules[pkg_str] = sys.modules['__main__']
             #We need to set __path__ because its needed for
             #relative importing
-            sys.modules[pkg_str].__path__ = [main_file_dir]
+            sys.modules[pkg_str].__path__ = [main_dir]
         else:
             #we need to import the package to be available
             importlib.import_module(pkg_str)
@@ -100,7 +130,10 @@ def init():
     # (in some cases relative_import could be called once from outside
     # __main__ if it was not called in __main__)
     # (also a reload of relative_import could trigger this function)
-    if main_globals.get('__package__') or main_globals.get('__name__') != '__main__':
+    if (main_globals.get('__package__') 
+        or main_globals.get('__name__') != '__main__'
+        or not main_globals.get('__file__')
+        ):
         return
     
     try:
--- run_tests.sh
+++ run_tests.sh
@@ -1,18 +1,43 @@
 #!/bin/bash
 
-args="-m unittest discover"
+args="-m unittest discover "
 
 #Checking in current platform
-python $args
+#eval "python $args $filter"
 
-python3 $args
+if [ "$1" == "wine" ] ; then
+pycmds="python
+python3
+wine python
+wine c:\\\\\\\\Python33\\\\\\\\python.exe"
+else
+pycmds="python
+python3"
+fi
+
+#Log executed commands
+function exec_cmd(){
+  out=$(eval "$*" 2>&1 )
+  ok=$(echo "$out" | tail -n 1)
+  if [[ "$ok" == "" || "$ok" == "OK" ]]; then
+    return 0
+  else
+    echo "--------------------"
+    echo "$*"
+    echo "$ok"
+    echo
+    #echo "$out"
+    return 1
+  fi
+}
 
 #We need to run each test as __main__, if not, we lose the testing
 for t in $(ls rel_imp_tests/*.py) ; do
-	python $t
-	python3 $t
-done 
+    exec_cmd "python $t"
+    exec_cmd "python3 $t"
+done
 
-#test in wine
-wine python $args
-wine c:\\Python33\\python.exe $args
+echo "$pycmds" | while read pcmd
+do
+  exec_cmd "$pcmd $args"
+done
