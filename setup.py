8c9cbc459a12421a439e4f917f4f3d68619b3443
jduo <joaduo@gmail.com>
2014-12-20 16:36:03 -0300

    Code in pep8. Move to version 0.2

--- setup.py
+++ setup.py
@@ -3,20 +3,21 @@ from setuptools import setup
 
 name = 'rel_imp'
 
+
 def long_description():
     with open('README', 'r') as f:
         return unicode(f.read())
 
 setup(
-  name = name,
-  py_modules = [name],
-  version = '0.1.3',
-  description = 'Enable explicit relative imports in __main__ module.',
-  long_description=long_description(),
-  author = 'Joaquin Duo',
-  author_email = 'joaduo@gmail.com',
-  license='MIT',
-  url = 'https://github.com/joaduo/'+name,
-  keywords = ['explicit', 'relative', 'import'],
-  install_requires=['importlib'],
+    name=name,
+    py_modules=[name],
+    version='0.2',
+    description='Enable explicit relative imports in __main__ module.',
+    long_description=long_description(),
+    author='Joaquin Duo',
+    author_email='joaduo@gmail.com',
+    license='MIT',
+    url='https://github.com/joaduo/' + name,
+    keywords=['explicit', 'relative', 'import'],
+    install_requires=['importlib'],
 )
