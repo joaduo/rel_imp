0f8f7dbf49baa8fe2703ea9ded4424ff171143ac
jduo <jduo@juju.com>
2020-08-19 03:36:00 -0300

    improve docs

--- relative_import.py
+++ relative_import.py
@@ -7,10 +7,19 @@ Code Licensed under MIT License. See LICENSE file.
 To enable explicit relative importing in __main__, you simply import
 this package before any relative import
 
-import relative_import
-from .my_pkg import foo, bar
+Usage:
+------
 
-There is no need to call any init function using this module
+To enable explicit relative importing in __main__, you simply import
+this package before any relative import
+
+    import relative_import
+    from .my_pkg import foo, bar
+    ...
+
+There is no need to call any init function using this module.
+Make sure your PYTHONPATH is correctly set to solve the relative path of the
+submodule/subpackage.
 '''
 
 import rel_imp
